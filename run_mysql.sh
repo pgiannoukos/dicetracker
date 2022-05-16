47abd53204e6cc80d7e46727a3666e4280e46388
Paris <p.giannoukos@gmail.com>
2022-05-16 18:40:59 +0300

    -

--- run_mysql.sh
+++ run_mysql.sh
@@ -1,4 +1,4 @@
 docker network create dicetracker-network || true
 docker kill mysql-dicetracker || true
 docker rm mysql-dicetracker || true
-docker run --name mysql-dicetracker -v "$(pwd)/mysql:/var/lib/mysql" --network dicetracker-network -e MYSQL_ROOT_PASSWORD=mysql -p 3306:3306 mysql:5.7
+docker run --name mysql-dicetracker --restart always -v "/var/database/mysql:/var/lib/mysql" --network dicetracker-network -e MYSQL_ROOT_PASSWORD=mysql -p 3306:3306 mysql:5.7
